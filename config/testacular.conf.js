18d3e08acd179e52ff2e2279694a8941e6e0a44f
Arthur Kay <arthurakay@gmail.com>
2013-04-09 13:35:36 -0500

    First of unit tests in place.

--- config/testacular.conf.js
+++ config/testacular.conf.js
@@ -4,8 +4,10 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
+  //'app/lib/angular/angular-*.js',
   'test/lib/angular/angular-mocks.js',
+
+  'app/js/app.js',
   'app/js/**/*.js',
   'test/unit/**/*.js'
 ];
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,31 +1,105 @@
 'use strict';
+beforeEach(module('todomvc'));
 
-/* jasmine specs for controllers go here */
+describe('TodoCtrl', function () {
+    var scope, ctrl;
 
-describe('MyCtrl1', function(){
-  var myCtrl1;
+    beforeEach(inject(function($rootScope, $controller) {
+        scope = $rootScope.$new();
+        ctrl = $controller(TodoCtrl, {$scope: scope});
+    }));
 
-  beforeEach(function(){
-    myCtrl1 = new MyCtrl1();
-  });
+    afterEach(function() {
+        ctrl = null;
+        scope = null;
+    });
 
+    describe('addTodo() method', function() {
 
-  it('should ....', function() {
-    //spec body
-  });
-});
-
-
-describe('MyCtrl2', function(){
-  var myCtrl2;
-
-
-  beforeEach(function(){
-    myCtrl2 = new MyCtrl2();
-  });
+        //sanity test
+        it('should be a function', function () {
+            expect(typeof scope.addTodo).toBe('function');
+        });
 
 
-  it('should ....', function() {
-    //spec body
-  });
+        it('should correctly add a "todo" to its internal stack', function () {
+            expect(typeof scope.todos).toBe('object');
+            expect(scope.todos.length).toEqual(0);
+
+            scope.newTodo = 'Foo';
+            scope.addTodo();
+
+            expect(scope.todos.length).toEqual(1);
+            //newTodo should be reset
+            expect(scope.newTodo).toEqual('');
+        });
+    });
+
+    describe('removeTodo() method', function() {
+
+        //sanity test
+        it('should be a function', function () {
+            expect(typeof scope.removeTodo).toBe('function');
+        });
+
+        it('should correctly remove a "todo" from its internal stack', function () {
+            var todo = 'Foo';
+
+            expect(typeof scope.todos).toBe('object');
+            expect(scope.todos.length).toEqual(0);
+
+            scope.newTodo = todo;
+            scope.addTodo();
+            expect(scope.todos.length).toEqual(1);
+
+            scope.removeTodo(todo);
+            expect(scope.todos.length).toEqual(0);
+        });
+    });
+
+    describe('markAll() method', function() {
+
+        //sanity test
+        it('should be a function', function () {
+            expect(typeof scope.markAll).toBe('function');
+        });
+
+        it('should correctly mark all todos on the stack as "completed = true"', function () {
+            var todo = 'Foo';
+
+            expect(typeof scope.todos).toBe('object');
+            expect(scope.todos.length).toEqual(0);
+
+            scope.newTodo = todo;
+            scope.addTodo();
+            expect(scope.todos.length).toEqual(1);
+
+            scope.markAll(true);
+            expect(scope.todos[0].completed).toEqual(true);
+        });
+    });
+
+    describe('clearDoneTodos() method', function() {
+
+        //sanity test
+        it('should be a function', function () {
+            expect(typeof scope.clearDoneTodos).toBe('function');
+        });
+
+        it('should correctly remove todos from its internal stack which are marked as "completed = true"', function () {
+            var todo = 'Foo';
+
+            expect(typeof scope.todos).toBe('object');
+            expect(scope.todos.length).toEqual(0);
+
+            scope.newTodo = todo;
+            scope.addTodo();
+            expect(scope.todos.length).toEqual(1);
+
+            scope.markAll(true);
+            scope.clearDoneTodos();
+            expect(scope.todos.length).toEqual(0);
+        });
+    });
+
 });
