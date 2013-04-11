fc146f6898a527a7bd301cbc0eac04be6190a308
Arthur Kay <arthurakay@gmail.com>
2013-04-11 13:18:37 -0500

    First crack at e2e tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,45 +1,23 @@
 'use strict';
+describe('todomvc', function () {
 
-/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+    beforeEach(function () {
+        localStorage.clear();
+        browser().navigateTo('../app/index.html');
+    });
 
-describe('my app', function() {
+    afterEach(function() {
 
-  beforeEach(function() {
-    browser().navigateTo('../../app/index.html');
-  });
-
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
-  });
-
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view1');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
+    describe('localstorage behavior', function() {
+        it('should load with zero items in localstorage', function() {
+            expect(repeater('#todo-list li').count()).toEqual(0);
+            input('newTodo').enter('Foo Bar');
+            expect(repeater('#todo-list li').count()).toEqual(1);
+        });
+
     });
 
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
